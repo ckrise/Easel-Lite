41940aba19beb0e8d3cd112125bb7da8f7978f8d
Bryan Cuneo <bryan.cuneo@gmail.com>
2018-04-26 11:18:53 -0500

    Routing for NewClass component

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { HomepageComponent } from './homepage/homepage.component'
 import { ClassesComponent } from './classes/classes.component'
 import { ClassDetailComponent } from './class-detail/class-detail.component'
 import { NotFoundComponent } from './not-found/not-found.component'
+import { NewClassComponent } from './new-class/new-class.component'
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'classes', component: ClassesComponent },
   { path: 'classes/:classid', component: ClassDetailComponent },
+  { path: 'new-class', component: NewClassComponent },
   // { path: '/*', component: NotFoundComponent },
 ];
 
