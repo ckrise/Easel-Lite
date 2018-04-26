974d69a641c60979943b1f8e5e12fc4e62344ce4
Caleb Krise <ckrise@harding.edu>
2018-04-26 15:58:31 -0500

    roster routing

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { ClassesComponent } from './classes/classes.component'
 import { ClassDetailComponent } from './class-detail/class-detail.component'
 import { NotFoundComponent } from './not-found/not-found.component'
 import { NewClassComponent } from './new-class/new-class.component'
+import { RosterComponent } from './roster/roster.component'
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'classes', component: ClassesComponent },
   { path: 'classes/:classid', component: ClassDetailComponent },
   { path: 'new-class', component: NewClassComponent },
-  // { path: '/*', component: NotFoundComponent },
+  { path: 'rosters/:classid', component: RosterComponent},
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
