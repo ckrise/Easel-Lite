b3ee50769ef9ba936a31876d226f98197c6157d6
Bryan Cuneo <bryan.cuneo@gmail.com>
2018-04-26 11:07:50 -0500

    NewClass component

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -9,7 +9,8 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { ClassesComponent } from './classes/classes.component';
 import { ApiService } from './api.service';
 import { ClassDetailComponent } from './class-detail/class-detail.component';
-import { NotFoundComponent } from './not-found/not-found.component'
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NewClassComponent } from './new-class/new-class.component'
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { NotFoundComponent } from './not-found/not-found.component'
     HomepageComponent,
     ClassesComponent,
     ClassDetailComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NewClassComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/client/app/new-class/new-class.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'el-new-class',
+  templateUrl: './new-class.component.html',
+  styleUrls: ['./new-class.component.css']
+})
+export class NewClassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
